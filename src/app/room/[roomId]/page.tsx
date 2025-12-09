f39cf76b4687f5027217de052d47413fab3e1e6d
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-12-09 20:30:06 +0530

    feat: Add date-fns dependency, enhance RoomPage with message fetching and real-time updates, and improve error handling on homepage.

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -2,19 +2,45 @@
 
 import { useUsername } from "@/hooks/useUsername";
 import { client } from "@/lib/client";
-import { useMutation } from "@tanstack/react-query";
-import { useParams } from "next/navigation";
-import { useRef, useState } from "react";
+import { useRealtime } from "@/lib/realtime-client";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { format } from "date-fns";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 
 const RoomPage = () => {
   const { username } = useUsername();
   const params = useParams();
   const roomId = params.roomId as string;
+  const router = useRouter();
 
   const [copyStatus, setCopyStatus] = useState("COPY");
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
   const [input, setInput] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const { data: messages, refetch } = useQuery({
+    queryKey: ["messages", roomId],
+
+    queryFn: async () => {
+      const res = await client.messages.get({ query: { roomId } });
+      return res.data;
+    },
+  });
+
+  useRealtime({
+    channels: [roomId],
+    events: ["chat.message", "chat.destroy"],
+    onData: ({ event }) => {
+      if (event === "chat.message") {
+        refetch();
+      }
+
+      if (event === "chat.destroy") {
+        router.push("/?destroyed=true");
+      }
+    },
+  });
 
   const { mutate: sendMessage, isPending } = useMutation({
     mutationFn: async ({ text }: { text: string }) => {
@@ -25,6 +51,7 @@ const RoomPage = () => {
         },
         { query: { roomId } }
       );
+      setInput("");
     },
   });
 
@@ -86,7 +113,45 @@ const RoomPage = () => {
         </button>
       </header>
 
-      <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin"></div>
+      {/* Messages */}
+      <div className="flex-1 p-4 space-y-4 overflow-y-scroll">
+        {messages?.messages.length === 0 && (
+          <div className="flex items-center justify-center h-full">
+            <p className="text-zinc-600 text-sm font-mono">
+              No messages yet, start the conversation
+            </p>
+          </div>
+        )}
+
+        {messages?.messages.map((msg) => (
+          <div
+            key={msg.id}
+            className={`flex flex-col  ${
+              msg.sender === username ? "items-end" : "items-start"
+            }`}
+          >
+            <div className="max-w-[80%] p-2 group rounded-md border border-zinc-800">
+              <div className="flex items-baseline gap-3 mb-1">
+                <span
+                  className={`text-xs font-bold ${
+                    msg.sender === username ? "text-green-500" : "text-blue-500"
+                  }`}
+                >
+                  {msg.sender === username ? "YOU" : msg.sender}
+                </span>
+
+                <span className="text-[10px] text-zinc-600">
+                  {format(msg.timestamp, "HH:mm")}
+                </span>
+              </div>
+
+              <p className="text-sm text-zinc-300 leading-relaxed break-all">
+                {msg.text}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
 
       <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
         <div className="flex gap-4">
@@ -118,7 +183,6 @@ const RoomPage = () => {
             onClick={() => {
               sendMessage({ text: input });
               inputRef.current?.focus();
-              setInput("");
             }}
             disabled={!input.trim() || isPending}
           >
